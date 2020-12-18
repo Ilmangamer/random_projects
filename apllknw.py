79302180cb6c1df13a4651fbbbf60c23434a9371
Ilmangamer <73790983+Ilmangamer@users.noreply.github.com>
2020-12-18 21:04:03 +0100

    Add files via upload

---
+++ apllknw.py
@@ -0,0 +1,24 @@
+
+def addicts(x, y):
+	if x != y:
+		return(x-1)
+		
+		while True:
+			if x >= y:
+				print(x)
+				for x in range(x):
+					print(x-1)
+		else:
+			for y in range(y):
+				print(y-1)
+print(addicts(5, 10))
+
+
+
+# a generator is a function that returns an iterator
+# itertor is an object that can be iterated upon
+# itertor is an object that can be iterated upon an object which will return data, one element at the the time
+
+
+	
+
